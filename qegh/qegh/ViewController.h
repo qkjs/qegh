81105691f75b4d875ce99d81e3c7590e04eb2ad4
Bernie Suen <bernie@Kingcrab.local>
2015-08-14 23:37:05 +0800

    init

---
+++ qegh/qegh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qegh
+//
+//  Created by Bernie Suen on 15/8/14.
+//  Copyright (c) 2015年 Bernie Suen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qegh/qegh/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  qegh
+//
+//  Created by Bernie Suen on 15/8/14.
+//  Copyright (c) 2015年 Bernie Suen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
